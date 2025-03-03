9a88b4e9a18888f727d89e6a856b6e06041c768e
shreyas <kyadashreyas@gmail.com>
2025-03-03 11:52:16 +0530

    change testimonial index key

--- src/modules/Home/components/Testimonial/Testimonial.tsx
+++ src/modules/Home/components/Testimonial/Testimonial.tsx
@@ -100,8 +100,8 @@ const Testimonial = () => {
 
       <div className="relative flex w-[100%] max-w-6xl flex-col pb-10 sm:flex md:hidden">
         <Slider {...slickSettings}>
-          {testimonials.map((testimonial: ITestimonial) => (
-            <div className="w-full px-2">
+          {testimonials.map((testimonial: ITestimonial, index) => (
+            <div className="w-full px-2" key={index}>
               <div
                 key={testimonial.id}
                 className={`font-Lato relative h-[428px] overflow-hidden rounded-[32px] border border-[#CFE1E8] bg-[#F3F7F9]`}
